bd780bff974b40706d8acb524a575449dc1d87d2
devken0 <119792185+devken0@users.noreply.github.com>
2025-07-05 17:16:29 +0800

    Add check-system

---
+++ check-system.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+OS_NAME=$(awk -F= '/^NAME=/ { gsub(/"/, "", $2); print $2 }' /etc/os-release)
+OS_VERSION=$(lsb_release -rs)
+ARCH=$(uname -m)
+
+check_system(){
+	if [ $OS_NAME != "Ubuntu" ]; then
+		return 1 
+	fi
+
+	# lt means less than 
+	if dpkg --compare-versions "$OS_VERSION" lt "22.04"; then
+		return 1
+	fi
+
+	if [ $ARCH != "x86_64" ]; then
+		return 1
+	fi
+}
+
+check_system
+
+if [ $? = 1 ]; then 
+	echo "The Linux version of Bedrock Dedicated Server requires Ubuntu 22.04 (LTS version) or later. Other distributions are not supported."
+else 
+	echo "Done compatibility checks. Downloading server software.."
+fi
